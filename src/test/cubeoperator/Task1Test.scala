127c3158b042ee80b66b250ec01f94ad2f0f0c39
Peilin Kang <kangpl@tsf-444-wpa-7-039.epfl.ch>
2019-04-21 19:49:15 +0200

    different # of reducer

--- src/test/cubeoperator/Task1Test.scala
+++ src/test/cubeoperator/Task1Test.scala
@@ -16,19 +16,19 @@ class Task1Test extends FlatSpec {
   val ctx = new SparkContext(sparkConf)
   val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
 
-  "Cube naive and MRCube" should "give same results for small dataset" in {
-    val reducers = 4
-    val cb = new CubeOperator(reducers)
+  //  "Cube naive and MRCube" should "give same results for small dataset" in {
+  //    val reducers = 4
+  //    val cb = new CubeOperator(reducers)
+  //
+  //    // Read in the datasets
+  //    //    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
+  //    val pathSmall = "../lineorder_small.tbl"
+  //    val (datasetSmall, dfSmall) = readResource(pathSmall)
+  //
+  //    inputSizeTest(cb, datasetSmall, dfSmall, "Small")
+  //  }
 
-    // Read in the datasets
-    //    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
-    val pathSmall = "../lineorder_small.tbl"
-    val (datasetSmall, dfSmall) = readResource(pathSmall)
-
-    inputSizeTest(cb, datasetSmall, dfSmall, "Small")
-  }
-
-  it should "give same results for medium dataset" in {
+  it should "give same results for medium dataset 4" in {
     val reducers = 4
     val cb = new CubeOperator(reducers)
 
@@ -40,17 +40,65 @@ class Task1Test extends FlatSpec {
 
   }
 
-//  it should "give same results for big dataset" in {
-//    val reducers = 4
-//    val cb = new CubeOperator(reducers)
-//
-//    //    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
-//    val pathBig = "../lineorder_big.tbl"
-//    val (datasetBig, dfBig) = readResource(pathBig)
-//
-//    inputSizeTest(cb, datasetBig, dfBig, "Big")
-//
-//  }
+  it should "give same results for medium dataset 8" in {
+    val reducers = 8
+    val cb = new CubeOperator(reducers)
+
+    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+    val pathMedium = "../lineorder_medium.tbl"
+    val (datasetMedium, dfMedium) = readResource(pathMedium)
+
+    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
+
+  }
+
+  it should "give same results for medium dataset 12" in {
+    val reducers = 12
+    val cb = new CubeOperator(reducers)
+
+    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+    val pathMedium = "../lineorder_medium.tbl"
+    val (datasetMedium, dfMedium) = readResource(pathMedium)
+
+    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
+
+  }
+
+  it should "give same results for medium dataset 16" in {
+    val reducers = 16
+    val cb = new CubeOperator(reducers)
+
+    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+    val pathMedium = "../lineorder_medium.tbl"
+    val (datasetMedium, dfMedium) = readResource(pathMedium)
+
+    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
+
+  }
+
+  it should "give same results for medium dataset 20" in {
+    val reducers = 20
+    val cb = new CubeOperator(reducers)
+
+    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+    val pathMedium = "../lineorder_medium.tbl"
+    val (datasetMedium, dfMedium) = readResource(pathMedium)
+
+    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
+
+  }
+
+  //  it should "give same results for big dataset" in {
+  //    val reducers = 4
+  //    val cb = new CubeOperator(reducers)
+  //
+  //    //    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
+  //    val pathBig = "../lineorder_big.tbl"
+  //    val (datasetBig, dfBig) = readResource(pathBig)
+  //
+  //    inputSizeTest(cb, datasetBig, dfBig, "Big")
+  //
+  //  }
 
   // Methods
   //  def currentMethodName(): String = Thread.currentThread.getStackTrace()(2).getMethodName
@@ -59,7 +107,7 @@ class Task1Test extends FlatSpec {
     val t0 = System.nanoTime()
     val result = block._2 // call-by-name
     val t1 = System.nanoTime()
-    println(block._1 + "'s execution time: " + (t1 - t0) /(1e9)+ "s")
+    println(block._1 + "'s execution time: " + (t1 - t0) / (1e9) + "s")
     result
   }
 
@@ -89,7 +137,7 @@ class Task1Test extends FlatSpec {
     val op = "SUM"
 
     // Evaluate and count execution time
-    val res_MRCube = timer { ("MRCube_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count() )}
+    val res_MRCube = timer { ("MRCube_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count()) }
     val res_cubeNaive = timer { ("cubeNaive_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op).count()) }
 
     // Compare results
