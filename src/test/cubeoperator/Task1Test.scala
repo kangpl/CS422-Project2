0d2b25ab01bcb411744a9241a2fed345383c832c
Peilin Kang <kangpl@tsf-444-wpa-7-039.epfl.ch>
2019-04-21 19:05:05 +0200

    test

--- src/test/cubeoperator/Task1Test.scala
+++ src/test/cubeoperator/Task1Test.scala
@@ -21,8 +21,8 @@ class Task1Test extends FlatSpec {
     val cb = new CubeOperator(reducers)
 
     // Read in the datasets
-    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
-    //  val pathSmall = "../lineorder_small.tbl"
+    //    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
+    val pathSmall = "../lineorder_small.tbl"
     val (datasetSmall, dfSmall) = readResource(pathSmall)
 
     inputSizeTest(cb, datasetSmall, dfSmall, "Small")
@@ -32,25 +32,25 @@ class Task1Test extends FlatSpec {
     val reducers = 4
     val cb = new CubeOperator(reducers)
 
-    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
-    //  val pathMedium = "../lineorder_medium.tbl"
+    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+    val pathMedium = "../lineorder_medium.tbl"
     val (datasetMedium, dfMedium) = readResource(pathMedium)
 
     inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
 
   }
 
-  it should "give same results for big dataset" in {
-    val reducers = 4
-    val cb = new CubeOperator(reducers)
-
-    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
-    //  val pathBig = "../lineorder_big.tbl"
-    val (datasetBig, dfBig) = readResource(pathBig)
-
-    inputSizeTest(cb, datasetBig, dfBig, "Big")
-
-  }
+//  it should "give same results for big dataset" in {
+//    val reducers = 4
+//    val cb = new CubeOperator(reducers)
+//
+//    //    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
+//    val pathBig = "../lineorder_big.tbl"
+//    val (datasetBig, dfBig) = readResource(pathBig)
+//
+//    inputSizeTest(cb, datasetBig, dfBig, "Big")
+//
+//  }
 
   // Methods
   //  def currentMethodName(): String = Thread.currentThread.getStackTrace()(2).getMethodName
@@ -59,13 +59,13 @@ class Task1Test extends FlatSpec {
     val t0 = System.nanoTime()
     val result = block._2 // call-by-name
     val t1 = System.nanoTime()
-    println(block._1 + "'s execution time: " + (t1 - t0) + "ns")
+    println(block._1 + "'s execution time: " + (t1 - t0) /(1e9)+ "s")
     result
   }
 
   def readResource(path: String): (Dataset, DataFrame) = {
-    val input = new File(path).getPath
-    //    val input = new File(getClass.getResource(path).getFile).getPath
+    //    val input = new File(path).getPath
+    val input = new File(getClass.getResource(path).getFile).getPath
 
     val df = sqlContext.read
       .format("com.databricks.spark.csv")
@@ -89,16 +89,12 @@ class Task1Test extends FlatSpec {
     val op = "SUM"
 
     // Evaluate and count execution time
-    val res_MRCube = timer { ("MRCube_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op)) }
-    val res_cubeNaive = timer { ("cubeNaive_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op)) }
+    val res_MRCube = timer { ("MRCube_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count() )}
+    val res_cubeNaive = timer { ("cubeNaive_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op).count()) }
 
     // Compare results
-    val diff_MRCube = res_MRCube.join(res_cubeNaive).collect {
-      case (k, (s1, s2)) if s1 == s2 => (k, s1, s2)
-    }
 
-    assert(diff_MRCube.count() == res_MRCube.count())
-    assert(diff_MRCube.count() == res_cubeNaive.count())
+    assert(res_MRCube == res_cubeNaive)
 
   }
 
