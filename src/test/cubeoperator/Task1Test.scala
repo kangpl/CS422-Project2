38819945deb75808338309c09f90b10bb83ce0fa
Sytta <sytta@live.ca>
2019-04-21 19:56:01 +0200

    Cube attribute tests

--- src/test/cubeoperator/Task1Test.scala
+++ src/test/cubeoperator/Task1Test.scala
@@ -16,41 +16,77 @@ class Task1Test extends FlatSpec {
   val ctx = new SparkContext(sparkConf)
   val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
 
-  "Cube naive and MRCube" should "give same results for small dataset" in {
+  //  "Cube naive and MRCube" should "give same results for small dataset" in {
+  //    val reducers = 4
+  //    val cb = new CubeOperator(reducers)
+  //
+  //    // Read in the datasets
+  //    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
+  //    //    val pathSmall = "../lineorder_small.tbl"
+  //    val (datasetSmall, dfSmall) = readResource(pathSmall)
+  //
+  //    inputSizeTest(cb, datasetSmall, dfSmall, "Small")
+  //  }
+  //
+  //  it should "give same results for medium dataset" in {
+  //    val reducers = 4
+  //    val cb = new CubeOperator(reducers)
+  //
+  //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+  //    //    val pathMedium = "../lineorder_medium.tbl"
+  //    val (datasetMedium, dfMedium) = readResource(pathMedium)
+  //
+  //    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
+  //
+  //  }
+  //
+  //  it should "give same results for big dataset" in {
+  //    val reducers = 4
+  //    val cb = new CubeOperator(reducers)
+  //
+  //    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
+  //    //    val pathBig = "../lineorder_big.tbl"
+  //    val (datasetBig, dfBig) = readResource(pathBig)
+  //
+  //    inputSizeTest(cb, datasetBig, dfBig, "Big")
+  //
+  //  }
+
+  val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
+  //    val pathMedium = "../lineorder_medium.tbl"
+  val (datasetMedium, dfMedium) = readResource(pathMedium)
+
+  "Cube naive and MRCube attribute test" should "give same results for 3 attributes" in {
     val reducers = 4
     val cb = new CubeOperator(reducers)
+    val groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate")
 
-    // Read in the datasets
-    //    val pathSmall = "/Users/yawen/Documents/Scala/lineorder_small.tbl"
-    val pathSmall = "../lineorder_small.tbl"
-    val (datasetSmall, dfSmall) = readResource(pathSmall)
-
-    inputSizeTest(cb, datasetSmall, dfSmall, "Small")
+    cubeAttributeTest(cb, datasetMedium, groupingList, "3")
   }
 
-  it should "give same results for medium dataset" in {
+  it should "give same results for 4 attributes" in {
     val reducers = 4
     val cb = new CubeOperator(reducers)
+    val groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate", "lo_orderkey")
 
-    //    val pathMedium = "/Users/yawen/Documents/Scala/lineorder_medium.tbl"
-    val pathMedium = "../lineorder_medium.tbl"
-    val (datasetMedium, dfMedium) = readResource(pathMedium)
-
-    inputSizeTest(cb, datasetMedium, dfMedium, "Medium")
-
+    cubeAttributeTest(cb, datasetMedium, groupingList, "4")
   }
 
-//  it should "give same results for big dataset" in {
-//    val reducers = 4
-//    val cb = new CubeOperator(reducers)
-//
-//    //    val pathBig = "/Users/yawen/Documents/Scala/lineorder_big.tbl"
-//    val pathBig = "../lineorder_big.tbl"
-//    val (datasetBig, dfBig) = readResource(pathBig)
-//
-//    inputSizeTest(cb, datasetBig, dfBig, "Big")
-//
-//  }
+  it should "give same results for 5 attributes" in {
+    val reducers = 4
+    val cb = new CubeOperator(reducers)
+    val groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate", "lo_orderkey", "lo_orderpriority")
+
+    cubeAttributeTest(cb, datasetMedium, groupingList, "5")
+  }
+  
+    it should "give same results for 6 attributes" in {
+    val reducers = 4
+    val cb = new CubeOperator(reducers)
+    val groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate", "lo_orderkey", "lo_orderpriority", "lo_shippriority")
+
+    cubeAttributeTest(cb, datasetMedium, groupingList, "6")
+  }
 
   // Methods
   //  def currentMethodName(): String = Thread.currentThread.getStackTrace()(2).getMethodName
@@ -59,13 +95,13 @@ class Task1Test extends FlatSpec {
     val t0 = System.nanoTime()
     val result = block._2 // call-by-name
     val t1 = System.nanoTime()
-    println(block._1 + "'s execution time: " + (t1 - t0) /(1e9)+ "s")
+    println(block._1 + "'s execution time: " + (t1 - t0) / (1e9) + "s")
     result
   }
 
   def readResource(path: String): (Dataset, DataFrame) = {
-    //    val input = new File(path).getPath
-    val input = new File(getClass.getResource(path).getFile).getPath
+    val input = new File(path).getPath
+    //    val input = new File(getClass.getResource(path).getFile).getPath
 
     val df = sqlContext.read
       .format("com.databricks.spark.csv")
@@ -89,8 +125,20 @@ class Task1Test extends FlatSpec {
     val op = "SUM"
 
     // Evaluate and count execution time
-    val res_MRCube = timer { ("MRCube_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count() )}
-    val res_cubeNaive = timer { ("cubeNaive_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op).count()) }
+    val res_MRCube = timer { ("MRCube_input_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count()) }
+    val res_cubeNaive = timer { ("cubeNaive_input_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op).count()) }
+
+    // Compare results
+
+    assert(res_MRCube == res_cubeNaive)
+
+  }
+
+  def cubeAttributeTest(cb: CubeOperator, dataset: Dataset, groupingList: List[String], size: String) {
+    val op = "SUM"
+    // Evaluate and count execution time
+    val res_MRCube = timer { ("MRCube_attribute_" + size, cb.cube(dataset, groupingList, "lo_supplycost", op).count()) }
+    val res_cubeNaive = timer { ("cubeNaive_attribute_" + size, cb.cube_naive(dataset, groupingList, "lo_supplycost", op).count()) }
 
     // Compare results
 
