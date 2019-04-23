39d1a56e56d2e374cb2bc247a76e3822f9c2f269
Peilin Kang <kangpl@Peilins-MacBook-Pro.local>
2019-04-24 01:26:03 +0200

    task2 simjoin reducer

--- src/main/scala/simjoin/Main.scala
+++ src/main/scala/simjoin/Main.scala
@@ -13,15 +13,15 @@ import java.io._
 
 object Main {
   def main(args: Array[String]) {
-    //    val inputFile="../dblp_small.csv"
+    val inputFile = "../dblp_1K.csv"
     val numAnchors = 4
     val distanceThreshold = 2
     val attrIndex = 0
 
-    val path = "/Users/yawen/Documents/Scala/dblp_small.csv"
-    val input = new File(path).getPath
+    //    val path = "/Users/yawen/Documents/Scala/dblp_small.csv"
+    //    val input = new File(path).getPath
 
-    //    val input = new File(getClass.getResource(inputFile).getFile).getPath
+    val input = new File(getClass.getResource(inputFile).getFile).getPath
     val sparkConf = new SparkConf().setAppName("CS422-Project2").setMaster("local[*]")
     val ctx = new SparkContext(sparkConf)
     val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
@@ -37,24 +37,24 @@ object Main {
     val schema = df.schema.toList.map(x => x.name)
     val dataset = new Dataset(rdd, schema)
 
-    //    val t1 = System.nanoTime
+    val t1 = System.nanoTime
     val sj = new SimilarityJoin(numAnchors, distanceThreshold)
     val res = sj.similarity_join(dataset, attrIndex)
-    //
-    //    val resultSize = res.count
-    //    println(resultSize)
-    //    val t2 = System.nanoTime
-    //
-    //    println((t2-t1)/(Math.pow(10,9)))
 
-    /*
+    val resultSize = res.count
+    println(resultSize)
+    val t2 = System.nanoTime
+
+    println((t2 - t1) / (Math.pow(10, 9)))
+
     // cartesian
     val t1Cartesian = System.nanoTime
     val cartesian = rdd.map(x => (x(attrIndex), x)).cartesian(rdd.map(x => (x(attrIndex), x)))
-                                   .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && edit_distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
+      .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && sj.edit_distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
 
     println(cartesian.count)
+    //    cartesian.foreach(x => println(x))
     val t2Cartesian = System.nanoTime
-    println((t2Cartesian-t1Cartesian)/(Math.pow(10,9))) */
+    println((t2Cartesian - t1Cartesian) / (Math.pow(10, 9)))
   }
 }
--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -3,6 +3,7 @@ package simjoin
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.Row
+import org.apache.spark.HashPartitioner
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -19,8 +20,6 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
   def similarity_join(dataset: Dataset, attrIndex: Int): RDD[(String, String)] = {
     val rdd = dataset.getRDD
     val anchors = rdd.takeSample(false, numAnchors).zipWithIndex
-    //    var homeArray: Array[String] = Array(null)
-    //    var rddResult: RDD[(String, String)] = null
 
     val get_partition = rdd.map(
       p => (
@@ -29,58 +28,46 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
           a =>
             edit_distance(p(attrIndex), a._1(attrIndex))).zipWithIndex.min))
 
-    //(Aaaaeldin M.sHsfez,(13,0))
-    //      get_partition.foreach(x => x._2.foreach(y => println(y)))
-
-    //    val SimilarityMapper = anchors.map(
-    //        a =>
-    //          get_partition.map(
-    //              row =>
-    //                if (row._2._2 == a._2)
-    //                  (a._2, "home", (row._1, "home"))
-    //                else if ((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1) && edit_distance(row._1, a._1(attrIndex)) < row._2._1 + 2 * distThreshold)
-    //                  (a._2, "outer", (row._1, "outer"))
-    //                else null
-    //      ).filter(x => x != null)
-    //      .sortBy(_._1))
-    //
-    //
-    //
-    //    SimilarityMapper.foreach(x => x.foreach(z=> println(z)))
-    //
-
     val SimilarityMapper = get_partition.flatMap(
       row =>
         anchors.map(
           a =>
             if (row._2._2 == a._2)
               (a._2, ("home", row._1))
-            else if ((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1) && edit_distance(row._1, a._1(attrIndex)) < row._2._1 + 2 * distThreshold)
+            else if (((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1)) && edit_distance(row._1, a._1(attrIndex)) <= row._2._1 + 2 * distThreshold)
               (a._2, ("outer", row._1))
             else null)).filter(x => x != null).sortBy(line => (line._1, line._2._1), ascending = true)
-
-    //    SimilarityMapper.foreach(x => println(x))
-
-    val SimilarityReducer = SimilarityMapper.reduceByKey(customFunc, 4)
-
-        SimilarityReducer.count()
-
-    //     val SimPair = SimilarityMapper.map( partition => partition.map( x =>
-    //       if (x._2._2 == "home") {
-    //         for(hp <- homeArray) {
-    //           if(edit_distance(hp, x._2._1) < distThreshold)
-    //         }
-    //         homeArray :+ x._2._1
-    //       }
-    //
-    null
+            
+    val rddPartitioned = SimilarityMapper.partitionBy(new HashPartitioner(numAnchors))
+        
+    val result = {
+      rddPartitioned.mapPartitionsWithIndex( (index, partitions) => {    
+        val joinResult = local_simjoin(partitions)
+        joinResult
+      })
+    }
+//    println(result.count())
+//    result.foreach(x => println(x))
+    result
   }
-
-  def customFunc: ((String, String), (String, String)) => (String, String) = {
-    (pair1: (String, String), pair2: (String, String)) =>
-      println(pair1._1 + " " + pair2._1)
-      pair1
-
+    
+  def local_simjoin(cluster:Iterator[(Int, (String, String))]) : Iterator[(String, String)] = {
+    var res = List[(String, String)]()
+    var homelist = List[String]()
+   
+        
+    while(cluster.hasNext) {
+      val point = cluster.next
+      for(prevPoint <- homelist) {
+        if(edit_distance(point._2._2, prevPoint) <= distThreshold)  {
+          res = res :+ (point._2._2, prevPoint)
+        }
+      }
+      if(point._2._1 == "home"){
+          homelist = homelist :+ point._2._2
+      }
+    }
+    res.iterator
   }
 
   def edit_distance(val1: Any, val2: Any): Int = {
