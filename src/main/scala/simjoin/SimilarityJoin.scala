f8390837eaba955c9ea579aca79786f2f3f8d04a
Sytta <sytta@live.ca>
2019-04-23 19:47:01 +0200

    Task2 - Doing Similarity join

--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -19,36 +19,74 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
   def similarity_join(dataset: Dataset, attrIndex: Int): RDD[(String, String)] = {
     val rdd = dataset.getRDD
     val anchors = rdd.takeSample(false, numAnchors).zipWithIndex
-//    anchors.foreach(x => println(x))
-    var homeArray: Array[String] = Array(null)
-    var rddResult: RDD[(String, String)] = null
+    //    var homeArray: Array[String] = Array(null)
+    //    var rddResult: RDD[(String, String)] = null
 
-    val get_partition = rdd.map(p => (
-      p(attrIndex).toString,
-      anchors.map(a => edit_distance(p(attrIndex).toString(), a._1(attrIndex).toString())).zipWithIndex.min))
+    val get_partition = rdd.map(
+      p => (
+        p(attrIndex).toString,
+        anchors.map(
+          a =>
+            edit_distance(p(attrIndex), a._1(attrIndex))).zipWithIndex.min))
+
     //(Aaaaeldin M.sHsfez,(13,0))
-//      get_partition.foreach(x => x._2.foreach(y => println(y)))
-      
-    val SimilarityMapper = anchors.map(a => get_partition.map(row =>
-      if (row._2._2 == a._2) 
-        ((a._2, "home"), (row._1, "home"))
-      else if ((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1) && edit_distance(row._1, a._1(attrIndex).toString()) < row._2._1 + 2*distThreshold) 
-        ((a._2, "outer"), (row._1, "outer"))
-      else null).filter(x => x != null).sortBy(_._1))
+    //      get_partition.foreach(x => x._2.foreach(y => println(y)))
 
-//    SimilarityMapper.foreach(x => x.foreach(z=> println(z)))
-     val SimPair = SimilarityMapper.map( partition => partition.map( x =>
-       if (x._2._2 == "home") { 
-         for(hp <- homeArray) {
-           if(edit_distance(hp, x._2._1) < distThreshold)
-         }
-         homeArray :+ x._2._1
-       }
-//      
+    //    val SimilarityMapper = anchors.map(
+    //        a =>
+    //          get_partition.map(
+    //              row =>
+    //                if (row._2._2 == a._2)
+    //                  (a._2, "home", (row._1, "home"))
+    //                else if ((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1) && edit_distance(row._1, a._1(attrIndex)) < row._2._1 + 2 * distThreshold)
+    //                  (a._2, "outer", (row._1, "outer"))
+    //                else null
+    //      ).filter(x => x != null)
+    //      .sortBy(_._1))
+    //
+    //
+    //
+    //    SimilarityMapper.foreach(x => x.foreach(z=> println(z)))
+    //
+
+    val SimilarityMapper = get_partition.flatMap(
+      row =>
+        anchors.map(
+          a =>
+            if (row._2._2 == a._2)
+              (a._2, ("home", row._1))
+            else if ((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1) && edit_distance(row._1, a._1(attrIndex)) < row._2._1 + 2 * distThreshold)
+              (a._2, ("outer", row._1))
+            else null)).filter(x => x != null).sortBy(line => (line._1, line._2._1), ascending = true)
+
+    //    SimilarityMapper.foreach(x => println(x))
+
+    val SimilarityReducer = SimilarityMapper.reduceByKey(customFunc, 4)
+
+        SimilarityReducer.count()
+
+    //     val SimPair = SimilarityMapper.map( partition => partition.map( x =>
+    //       if (x._2._2 == "home") {
+    //         for(hp <- homeArray) {
+    //           if(edit_distance(hp, x._2._1) < distThreshold)
+    //         }
+    //         homeArray :+ x._2._1
+    //       }
+    //
     null
   }
-  
-  def edit_distance(str1: String, str2: String): Int = {
+
+  def customFunc: ((String, String), (String, String)) => (String, String) = {
+    (pair1: (String, String), pair2: (String, String)) =>
+      println(pair1._1 + " " + pair2._1)
+      pair1
+
+  }
+
+  def edit_distance(val1: Any, val2: Any): Int = {
+    val str1 = val1.toString
+    val str2 = val2.toString
+
     val lenStr1 = str1.length
     val lenStr2 = str2.length
 
