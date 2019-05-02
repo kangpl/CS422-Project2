b9dc96486527a21a78cb4e913f0262ea1097c2e7
Peilin Kang <kangpl@tsf-444-wpa-6-068.epfl.ch>
2019-05-02 12:02:06 +0200

    use var rdd

--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -18,15 +18,15 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
    * that attribute.
    * */
   def similarity_join(dataset: Dataset, attrIndex: Int): RDD[(String, String)] = {
-    val rdd = dataset.getRDD
+    rdd = dataset.getRDD.map(row => row.getString(attrIndex))
     val anchors = rdd.takeSample(false, numAnchors).zipWithIndex
 
     val get_partition = rdd.map(
       p => (
-        p(attrIndex).toString,
+        p,
         anchors.map(
           a =>
-            edit_distance(p(attrIndex), a._1(attrIndex))).zipWithIndex.min))
+            edit_distance(p, a._1)).zipWithIndex.min))
 
     val SimilarityMapper = get_partition.flatMap(
       row =>
@@ -34,7 +34,7 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
           a =>
             if (row._2._2 == a._2)
               (a._2, ("home", row._1))
-            else if (((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1)) && edit_distance(row._1, a._1(attrIndex)) <= row._2._1 + 2 * distThreshold)
+            else if (((row._2._2 < a._2) ^ ((row._2._2 + a._2) % 2 == 1)) && edit_distance(row._1, a._1) <= row._2._1 + 2 * distThreshold)
               (a._2, ("outer", row._1))
             else null)).filter(x => x != null).sortBy(line => (line._1, line._2._1), ascending = true)
             
@@ -50,7 +50,7 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
 //    result.foreach(x => println(x))
     result
   }
-    
+  
   def local_simjoin(cluster:Iterator[(Int, (String, String))]) : Iterator[(String, String)] = {
     var res = List[(String, String)]()
     var homelist = List[String]()  
@@ -60,6 +60,7 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
       for(prevPoint <- homelist) {
         if(edit_distance(point._2._2, prevPoint) <= distThreshold)  {
           res = res :+ (point._2._2, prevPoint)
+          res = res :+ (prevPoint, point._2._2)
         }
       }
       if(point._2._1 == "home"){
@@ -70,12 +71,10 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
     res.iterator
   }
 
-  def edit_distance(val1: Any, val2: Any): Int = {
-    val str1 = val1.toString
-    val str2 = val2.toString
+  def edit_distance(val1: String, val2: String): Int = {
 
-    val lenStr1 = str1.length
-    val lenStr2 = str2.length
+    val lenStr1 = val1.length
+    val lenStr2 = val2.length
 
     val d: Array[Array[Int]] = Array.ofDim(lenStr1 + 1, lenStr2 + 1)
 
@@ -83,7 +82,7 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
     for (j <- 0 to lenStr2) d(0)(j) = j
 
     for (i <- 1 to lenStr1; j <- 1 to lenStr2) {
-      val cost = if (str1(i - 1) == str2(j - 1)) 0 else 1
+      val cost = if (val1(i - 1) == val2(j - 1)) 0 else 1
 
       d(i)(j) = List(
         d(i - 1)(j) + 1, // deletion
@@ -95,4 +94,3 @@ class SimilarityJoin(numAnchors: Int, distThreshold: Int) extends java.io.Serial
     d(lenStr1)(lenStr2)
   }
 }
-
