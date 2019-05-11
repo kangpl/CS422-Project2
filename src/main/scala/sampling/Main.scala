b41312c73081b6698c3304549bb34bdb9a01fc8a
Peilin Kang <kangpl@tsf-444-wpa-3-080.epfl.ch>
2019-05-11 17:54:35 +0200

    scasrs

--- src/main/scala/sampling/Main.scala
+++ src/main/scala/sampling/Main.scala
@@ -23,40 +23,13 @@ object Main {
     //      Nil
     //    ))
 
-    val path = "/Users/yawen/Documents/Scala/lineitem.tbl"
-    val input = new File(path).getPath
-    
-//    val inputFile = "../lineitem.tbl"
-//    val input = new File(getClass.getResource(inputFile).getFile).getPath
-
+//    val path = "/Users/yawen/Documents/Scala/lineitem.tbl"
+//    val input = new File(path).getPath
     val sparkConf = new SparkConf().setAppName("CS422-Project2").setMaster("local[16]")
     val ctx = new SparkContext(sparkConf)
     val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
     
-    val customSchema = StructType(Array(
-    StructField("l_orderkey", LongType, true),
-    StructField("l_partkey", LongType, true),
-    StructField("l_suppkey", LongType, true),
-    StructField("l_linenumber", IntegerType, true),
-    StructField("l_quantity", DoubleType, true),
-    StructField("l_extendedprice", DoubleType, true),
-    StructField("l_discount", DoubleType, true),
-    StructField("l_tax", DoubleType, true),
-    StructField("l_returnflag", StringType, true),
-    StructField("l_linestatus", StringType, true),
-    StructField("l_shipdate", DateType, true),
-    StructField("l_commitdate", DateType, true),
-    StructField("l_receiptdate", DateType, true),
-    StructField("l_shipinstruct", StringType, true),
-    StructField("l_shipmode", StringType, true),
-    StructField("l_comment", StringType, true)))
-    
-    val lineitem = sqlContext.read
-      .format("com.databricks.spark.csv")
-      .option("header", "false")
-      .option("delimiter", "|")
-      .schema(customSchema)
-      .load(input)
+    val lineitem = sqlContext.read.load("../CS422-Project2/src/main/resources/tpch_parquet_sf1/lineitem.parquet")
       
 //    val customer = sqlContext.read.load("/cs422-data/tpch/sf100/parquet/customer.parquet")
 //    val nation = sqlContext.read.load("/cs422-data/tpch/sf100/parquet/nation.parquet")
@@ -66,21 +39,17 @@ object Main {
 //    val partsupp = sqlContext.read.load("/cs422-data/tpch/sf100/parquet/partsupp.parquet")
 //    val region = sqlContext.read.load("/cs422-data/tpch/sf100/parquet/region.parquet")
 //    val supplier = sqlContext.read.load("/cs422-data/tpch/sf100/parquet/supplier.parquet")
-//  
+      
+    var desc_ = new Description
+    desc_.lineitem = lineitem
+    desc_.e = 0.1
+    desc_.ci = 0.95
     lineitem.show(5)
-    
-    print("Schema:")
     lineitem.printSchema()
-      
-      
-      
+    
 
-//    var desc_ = new Description
-//    desc_.lineitem = lineitem
-//    desc_.e = 0.1
-//    desc_.ci = 0.95
-//    
-//    val tmp = Sampler.sample(desc_.lineitem, 1000000, desc_.e, desc_.ci)
+
+    val tmp = Sampler.sample(desc_.lineitem, 1000000, desc_.e, desc_.ci, ctx)
     //    desc.samples = tmp._1
     //    desc.sampleDescription = tmp._2
     //
--- src/main/scala/sampling/Sampler.scala
+++ src/main/scala/sampling/Sampler.scala
@@ -1,61 +1,87 @@
 package sampling
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql._
+import org.apache.spark.HashPartitioner
+import scala.math._
 
 object Sampler {
-  def sample(lineitem: DataFrame, storageBudgetBytes: Long, e: Double, ci: Double): (List[RDD[_]], _) = {
+  def sample(lineitem: DataFrame, storageBudgetBytes: Long, e: Double, ci: Double, ctx: SparkContext): (List[RDD[_]], _) = {
     // TODO: implement
+    val rowBytes = lineitem.schema.map(x => x.dataType.defaultSize).reduce((a, b) => a+b)
+    
     val schema = lineitem.schema.map(x => x.name)
-//    val QCSs_index = List(List(0,2))
-//    val QCSs = QCSs_index.map(qcs => qcs.map(x => schema(x)))
-    val K = 100
-    val sigma = 0.00005
-    val res = lineitem.groupBy("_c8", "_c9", "_c10").count()
-    res.show(1000)
+    val totalQcs = List(
+        List("l_returnflag", "l_linestatus", "l_shipdate"),                                //List(8, 9, 10)
+        List("l_orderkey", "l_shipdate"),                                                  //List(0, 10)
+        List("l_orderkey", "l_suppkey"),                                                   //List(0, 2)
+        List("l_quantity", "l_discount", "l_shipdate"),                                    //List(4, 6, 10) 
+        List("l_orderkey", "l_suppkey", "l_shipdate"),                                     //List(0, 2, 10)
+        List("l_orderkey", "l_partkey", "l_suppkey"),                                      //List(0, 1, 2)
+        List("l_orderkey", "l_returnflag"),                                                //List(0, 8)
+        List("l_orderkey", "l_shipdate", "l_commitdate", "l_receiptdate", "l_shipmode"),   //List(0, 10, 11, 12, 14)
+        List("l_partkey", "l_quantity"),                                                   //List(1, 4)
+        List("l_orderkey", "l_quantity"),                                                  //List(0, 4)
+        List("l_partkey", "l_quantity", "l_shipinstruct", "l_shipmode"),                   //List(1, 4, 13, 14)
+        List("l_partkey", "l_suppkey", "l_shipdate"))                                      //List(1, 2, 10)   
+    val totalQcsIndex = totalQcs.map(qcs => qcs.map(q => schema.indexOf(q)))
     
-    
-    // 0) ORDERKEY, 1) PARTKEY, 2) SUPPKEY, 3) LINENUMBER, 4) QUANTITY, 5) EXTENDEDPRICE, 6) DISCOUNT, 7) TAX, 8) RETURNFLAG, 
-    // 9) LINESTATUS, 10) SHIPDATE, 11) COMMITDATE, 12) RECEIPTDATE, 13) SHIPINSTRUCT, 14) SHIPMODE, 15) COMMENT
-    
-    //Schema:root
-    // |-- l_orderkey: long (nullable = true)
-    // |-- l_partkey: long (nullable = true)
-    // |-- l_suppkey: long (nullable = true)
-    // |-- l_linenumber: integer (nullable = true)
-    // |-- l_quantity: decimal(12,2) (nullable = true)
-    // |-- l_extendedprice: decimal(12,2) (nullable = true)
-    // |-- l_discount: decimal(12,2) (nullable = true)
-    // |-- l_tax: decimal(12,2) (nullable = true)
-    // |-- l_returnflag: string (nullable = true)
-    // |-- l_linestatus: string (nullable = true)
-    // |-- l_shipdate: date (nullable = true)
-    // |-- l_commitdate: date (nullable = true)
-    // |-- l_receiptdate: date (nullable = true)
-    // |-- l_shipinstruct: string (nullable = true)
-    // |-- l_shipmode: string (nullable = true)
-    // |-- l_comment: string (nullable = true)
-    
-    // Calculate K
-    /*QCS
-    {8, 9, 10} 					l_shipdate l_returnflag l_linestatus
-    {0, 10} 							l_orderkey l_shipdate
-    {0, 2} 							l_orderkey l_suppkey
-    {4, 6, 10} 					l_shipdate l_discount l_quantity 
-    {0, 2, 10} 					l_orderkey l_suppkey l_shipdate
-    {0, 1, 2} 						l_orderkey l_suppkey l_partkey 
-    {0, 8} 							l_orderkey l_returnflag
-    {0, 10, 11, 12, 14} 	l_orderkey l_shipmode l_commitdate l_receiptdate l_shipdate
-    {1, 4} 							l_partkey l_quantity
-    {0, 4} 							l_orderkey l_quantity
-    {1, 4, 13, 14} 				l_partkey l_quantity l_shipmode l_shipinstruct
-    {1, 2, 10}						l_partkey l_suppkey l_shipdate
-    */
-    
-    //
-    
-    
-//    lineitem.groupby("col_
+    val lineitemRdd = ctx.parallelize(lineitem.take(100000))
+//    val lineitemRdd = lineitem.rdd
+    //calculate minimum k for each query column set
+    val qcsWithKey = lineitemRdd.map(row => (totalQcsIndex(1).map(x => row(x)).mkString("_"), row))  
+//    var minStrataSize = 1
+//    var maxStrataSize = qcsWithKey.groupByKey.map(x => x._2.size).max
+//    while(minStrataSize <= maxStrataSize){
+//      var k = (minStrataSize + maxStrataSize) / 2
+//      val groupedQcs = qcsWithKey.groupByKey
+//      val newGroupedQcs = groupedQcs.map( x => (x._1, x._2, calSampleSize(x._2, k)))
+//      val totalSampleSize = newGroupedQcs.map(x => x._3).reduce((a, b) => a + b)
+//      newGroupedQcs.map( x => calculate(x._2, x._3, totalSampleSize) )  
+//    }
+    val K = 1
+    val stratifiedSample = qcsWithKey.groupByKey.map(x => (x._1, (x._2.size, x._2))).flatMap(x =>ScaSRS(x._2, K))
+    print(stratifiedSample.count())
+    print(qcsWithKey.keys.distinct.count.toInt)
     null
+  }
+  
+  def ScaSRS(size_stratum: (Int, Iterable[Row]), K: Int) : Iterable[Row] = {
+    val sigma = 0.00000001
+    val stratumSize = size_stratum._1
+    val stratum = size_stratum._2.toIterator
+    val r = scala.util.Random
+    
+    if (stratumSize < K) stratum.toIterable
+    else {
+      val p = K.toDouble / stratumSize.toDouble
+      val gamma1 = -1.0 * (log(sigma) / stratumSize)
+      val gamma2 = -1.0 * (2 * log(sigma) / 3 / stratumSize)
+      val q1 = min(1, p + gamma1 + sqrt(gamma1 * gamma1 + 2 * gamma1 * p))
+      val q2 = max(0, p + gamma2 - sqrt(gamma2 * gamma2 + 3 * gamma2 * p))
+      
+      var l = 0
+      var waitlist = List[(Double, Row)]()
+      var res = List[Row]()
+      
+      while(stratum.toIterator.hasNext) {
+        val nextRow = stratum.next
+        val Xj = r.nextDouble
+        if(Xj < q2) { 
+          res = res :+ nextRow
+          l += 1
+        }
+        else if(Xj < q1){
+          waitlist = waitlist :+ (Xj, nextRow)
+        }
+      }
+      
+      //select the smallest pn-l items from waitlist
+      waitlist = waitlist.sortBy(_._1)
+      if(p * stratumSize - l > 0) res = res ++ waitlist.take(ceil((p * stratumSize)).toInt - l).map(_._2)
+      res
+    }
   }
 }
