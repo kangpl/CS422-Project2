c2b32ff5e2c25f3f9b908d2c7dcbf14962334643
Peilin Kang <kangpl@tsf-444-wpa-3-080.epfl.ch>
2019-05-12 19:11:02 +0200

    check error bound

--- src/main/scala/sampling/Sampler.scala
+++ src/main/scala/sampling/Sampler.scala
@@ -12,7 +12,7 @@ object Sampler {
   def sample(lineitem: DataFrame, storageBudgetBytes: Long, e: Double, ci: Double): (List[RDD[_]], _) = {
     // TODO: implement
     val schema = lineitem.schema.map(x => x.name)
-    
+
     // total query column sets appear in TPC-H queries
     val totalQcs = List(
       List("l_returnflag", "l_linestatus", "l_shipdate"), //List(8, 9, 10)
@@ -28,73 +28,92 @@ object Sampler {
       List("l_partkey", "l_quantity", "l_shipinstruct", "l_shipmode"), //List(1, 4, 13, 14)
       List("l_partkey", "l_suppkey", "l_shipdate")) //List(1, 2, 10)
     val totalQcsIndex = totalQcs.map(qcs => qcs.map(q => schema.indexOf(q)))
-    
+
     // useful query column sets after hardcode
     val usefulQcs = List(
-        List("l_returnflag", "l_linestatus", "l_shipdate"),                                //List(8, 9, 10)          //0.64%   ///
-        List("l_quantity", "l_discount", "l_shipdate"),                                    //List(4, 6, 10)          //80.64%  ///
-        List("l_orderkey", "l_returnflag"),                                                //List(0, 8)              //34.49%  ///
-        List("l_partkey", "l_quantity"))                                                   //List(1, 4)              //75.18%  ///
+      List("l_returnflag", "l_linestatus", "l_shipdate"), //List(8, 9, 10)          //0.64%   ///
+      List("l_quantity", "l_discount", "l_shipdate"), //List(4, 6, 10)          //80.64%  ///
+      List("l_orderkey", "l_returnflag"), //List(0, 8)              //34.49%  ///
+      List("l_partkey", "l_quantity")) //List(1, 4)              //75.18%  ///
     val usefulQcsIndex = usefulQcs.map(qcs => qcs.map(q => schema.indexOf(q)))
     val attrIndex = schema.indexOf("l_extendedprice")
-    
+
     // according datatype to estimate the storage space of one tuple
-    val rowBytes = lineitem.schema.map(x => x.dataType.defaultSize).reduce((a, b) => a+b)
+    val rowBytes = lineitem.schema.map(x => x.dataType.defaultSize).reduce((a, b) => a + b)
     val storageBudgetTuples = storageBudgetBytes / rowBytes
-    
-    // calculate absolute error according to relative error and sum of l_extendedprice 
+
+    // calculate absolute error according to relative error and sum of l_extendedprice
     val sumValue = lineitem.agg(functions.sum("l_extendedprice")).first.getDouble(0)
     val errorBound = sumValue * e
-   
-//    val lineitemRdd = ctx.parallelize(lineitem.take(100000))
-//    val lineitemRdd = lineitem.rdd
-    
+
+    //    val lineitemRdd = ctx.parallelize(lineitem.take(100000))
+    val lineitemRdd = lineitem.rdd
+
     //calculate minimum k for each query column set
     val dfAgg = lineitem.groupBy("l_returnflag", "l_linestatus", "l_shipdate")
-                      .agg(functions.count("l_extendedprice"), functions.var_pop("l_extendedprice"))
-    val dfNewAgg = dfAgg.select(dfAgg("count(l_extendedprice)"), dfAgg("var_pop(l_extendedprice)"))
-    val rddAgg = dfNewAgg.rdd
+      .agg(functions.count("l_extendedprice"), functions.var_pop("l_extendedprice"))
+      .select("count(l_extendedprice)", "var_pop(l_extendedprice)")
+    val rddAgg = dfAgg.rdd
     val rddNewAgg = rddAgg.map(row => Row(row.getLong(0).toDouble, row.getDouble(1)))
-                      
-//    val qcsWithKey = lineitemRdd.map(row => (usefulQcsIndex(0).map(x => row(x)).mkString("_"), row)).groupByKey
+
     var minStrataSize = 1.0
     var maxStrataSize = dfAgg.agg(functions.max("count(l_extendedprice)")).first.getLong(0).toDouble
     var magicK = 0.0
-//    while(minStrataSize < maxStrataSize){
-//      var K = scala.math.floor((minStrataSize + maxStrataSize) / 2)
-      var K = 2
+
+    print("original maxStrataSize: ", maxStrataSize)
+    println("original minStrataSize: ", minStrataSize)
+
+    var foundMinK = false
+
+    while (!foundMinK) {
+      var satisfied = false
+      var K = scala.math.floor((minStrataSize + maxStrataSize) / 2)
       print("K: ", K)
       val stratumError = rddNewAgg.map(row => calError(row, K))
-      val stratumSum = stratumError.reduce(_+_)
-      print("stratumSum: ", stratumSum)
-      val totalError = scala.math.sqrt(stratumSum)*1.96
+      val stratumSum = stratumError.reduce(_ + _)
+      val totalError = scala.math.sqrt(stratumSum) * 1.96
       print("totalError: ", totalError, "errorBound", errorBound)
-      if(totalError <= errorBound)
-        maxStrataSize = K - 1
-      else
+      if (totalError <= errorBound) {
+        print("Satisfied")
+        maxStrataSize = K
+        satisfied = true
+      } else {
         minStrataSize = K + 1
-        
+        print("Not satisfied")
+      }
+
       print("maxStrataSize: ", maxStrataSize)
-      print("minStrataSize: ", minStrataSize)
-      magicK = K
-//    }
+      println("minStrataSize: ", minStrataSize)
+
+      if ((maxStrataSize == minStrataSize) && satisfied) {
+        magicK = K
+        foundMinK = true
+      }
+
+      // avoid loop
+      if ((maxStrataSize < minStrataSize) && !satisfied) {
+        magicK = maxStrataSize
+        foundMinK = true
+      }
+    }
     print(magicK)
-//    val K = 2
-//    val stratifiedSample = qcsWithKey.map(x => (x._1, (x._2.size, x._2))).flatMap(x =>ScaSRS(x._2, K))
-//    print(stratifiedSample.count())
-//    print(qcsWithKey.keys.distinct.count.toInt)
+    //    val K = 2
+    //    val qcsWithKey = lineitemRdd.map(row => (usefulQcsIndex(0).map(x => row(x)).mkString("_"), row)).groupByKey
+    //    val stratifiedSample = qcsWithKey.map(x => (x._1, (x._2.size, x._2))).flatMap(x =>ScaSRS(x._2, K))
+    //    print(stratifiedSample.count())
+    //    print(qcsWithKey.keys.distinct.count.toInt)
     null
   }
-  
-  def calError( x: Row, K: Double): Double = {
+
+  def calError(row: Row, K: Double): Double = {
     var sampleSize = K
-    val stratumSize = x.getDouble(0)
-    if(stratumSize < K) sampleSize = stratumSize
-    val error = scala.math.pow(stratumSize, 2)*x.getDouble(1)/sampleSize
+    val stratumSize = row.getDouble(0)
+    if (stratumSize < K) sampleSize = stratumSize
+    val error = scala.math.pow(stratumSize, 2) * row.getDouble(1) / sampleSize
     return error
   }
-  
-  def ScaSRS(size_stratum: (Int, Iterable[Row]), K: Int) : Iterable[Row] = {
+
+  def ScaSRS(size_stratum: (Int, Iterable[Row]), K: Int): Iterable[Row] = {
     val sigma = 0.00005
     val stratumSize = size_stratum._1
     val stratum = size_stratum._2.toIterator
@@ -107,12 +126,12 @@ object Sampler {
       val gamma2 = -1.0 * (2 * scala.math.log(sigma) / 3 / stratumSize)
       val q1 = scala.math.min(1, p + gamma1 + scala.math.sqrt(gamma1 * gamma1 + 2 * gamma1 * p))
       val q2 = scala.math.max(0, p + gamma2 - scala.math.sqrt(gamma2 * gamma2 + 3 * gamma2 * p))
-      
+
       var l = 0
       var waitlist = List[(Double, Row)]()
       var res = List[Row]()
-      
-      while(stratum.hasNext) {
+
+      while (stratum.hasNext) {
         val nextRow = stratum.next
         val Xj = r.nextDouble
         if (Xj < q2) {
@@ -125,7 +144,7 @@ object Sampler {
 
       //select the smallest pn-l items from waitlist
       waitlist = waitlist.sortBy(_._1)
-      if(p * stratumSize - l > 0) res = res ++ waitlist.take(scala.math.ceil((p * stratumSize)).toInt - l).map(_._2)
+      if (p * stratumSize - l > 0) res = res ++ waitlist.take(scala.math.ceil((p * stratumSize)).toInt - l).map(_._2)
       res
     }
   }
