e70929d734cf1f29a4650f9a5e5fbbae570e2636
Sytta <sytta@live.ca>
2019-05-18 00:28:32 +0200

    Remove inutile parts

--- src/main/scala/simjoin/Main.scala
+++ src/main/scala/simjoin/Main.scala
@@ -16,105 +16,31 @@ object Main {
     val onCluster = true
 
     // Spark environment
-//    val sparkConf = new SparkConf().setAppName("CS422-Project2")//.setMaster("local[*]")
-//    val ctx = new SparkContext(sparkConf)
-//    val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
-//
-//    val pathSmall = "dblp_1k.csv"
-//    val (datasetSmall, rddSmall) = readResource(pathSmall, onCluster, sqlContext)
-//
-//    val pathMedium = "dblp_3k.csv"
-//    val (datasetMedium, rddMedium) = readResource(pathMedium, onCluster, sqlContext)
-//
-//    val pathMedium2 = "dblp_5k.csv"
-//    val (datasetMedium2, rddMedium2) = readResource(pathMedium2, onCluster, sqlContext)
-//
-//    val pathBig = "dblp_10k.csv"
-//    val (datasetBig, rddBig) = readResource(pathBig, onCluster, sqlContext)
-//
-//    testingInputSize(datasetSmall, rddSmall, "1K")
-//    testingInputSize(datasetMedium, rddMedium, "3K")
-//    testingInputSize(datasetMedium2, rddMedium2, "5K")
-//    testingInputSize(datasetBig, rddBig, "10K")
-//
-//    testingAnchorSize(datasetSmall, rddSmall, 4)
-//    testingAnchorSize(datasetSmall, rddSmall, 10)
-//    testingAnchorSize(datasetSmall, rddSmall, 20)
-//    testingAnchorSize(datasetSmall, rddSmall, 50)
+    val sparkConf = new SparkConf().setAppName("CS422-Project2")//.setMaster("local[*]")
+    val ctx = new SparkContext(sparkConf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
 
-        val inputFile = "../dblp_1k.csv"
-        val numAnchors = 4
-        val distanceThreshold = 2
-        val attrIndex = 0
-        var output = ""
-    
-//        val path = "/Users/yawen/Documents/Scala/dblp_small.csv"
-    //    val inputFile = new File(path).getPath
-    
-        val input = new File(getClass.getResource(inputFile).getFile).getPath
-        val sparkConf = new SparkConf().setAppName("CS422-Project2").setMaster("local[*]")
-        val ctx = new SparkContext(sparkConf)
-        val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
-    
-        val df = sqlContext.read
-          .format("com.databricks.spark.csv")
-          .option("header", "true")
-          .option("inferSchema", "true")
-          .option("delimiter", ",")
-          .load(input)
-    
-        val rdd = df.rdd
-        val schema = df.schema.toList.map(x => x.name)
-        val dataset = new Dataset(rdd, schema)
-    
-        val t1 = System.nanoTime
-        val sj = new SimilarityJoin(numAnchors, distanceThreshold)
-        val res = sj.similarity_join(dataset, attrIndex)
-    
-        val resultSize = res.count
-        println("Cluster join count: " + resultSize)
-        val t2 = System.nanoTime
-    
-//        val clusterResults = res.map( x=>
-//                                          if (x._1 < x._2)
-//                                            (x._1, x._2)
-//                                          else
-//                                            (x._2, x._1))
-//                                 .sortBy(line => (line._1, line._2))
-//    
-//        output += "Cluster join result: \n"
-//        for (x <- clusterResults.collect()) output += x.toString() + "\n"
-//    
-        output = "Cluster join: " + (t2 - t1) / (Math.pow(10, 9)) + "s" + "\n\n"
-    
-        // cartesian
-        val t1Cartesian = System.nanoTime
-        val cartesian = rdd.map(x => (x(attrIndex), x)).cartesian(rdd.map(x => (x(attrIndex), x)))
-          .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && sj.edit_distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
-    
-        println("Cartesian count: " + cartesian.count)
-        val t2Cartesian = System.nanoTime
-    
-    
-//        val cartesianResults = cartesian.asInstanceOf[RDD[((String, Row), (String, Row))]]
-//                                        .map( x=> (x._1._1, x._2._1))
-//                                        .map( x=>
-//                                                if (x._1 < x._2)
-//                                                  (x._1, x._2)
-//                                                else
-//                                                  (x._2, x._1)
-//                                          )
-//                                          .sortBy(line => (line._1, line._2), ascending = true)
-    
-    
-    
-    
-//        output += "Cartesian join result: \n"
-//        for (y <- cartesianResults.collect()) output += y.toString() + "\n"
-//        output += "Cartesian join: " + (t2Cartesian - t1Cartesian) / (Math.pow(10, 9)) + "s \n"
-        output += "Cartesian join: " + (t2Cartesian - t1Cartesian) / (Math.pow(10, 9)) + "s \n"
-    
-        print(output)
+    val pathSmall = "dblp_1k.csv"
+    val (datasetSmall, rddSmall) = readResource(pathSmall, onCluster, sqlContext)
+
+    val pathMedium = "dblp_3k.csv"
+    val (datasetMedium, rddMedium) = readResource(pathMedium, onCluster, sqlContext)
+
+    val pathMedium2 = "dblp_5k.csv"
+    val (datasetMedium2, rddMedium2) = readResource(pathMedium2, onCluster, sqlContext)
+
+    val pathBig = "dblp_10k.csv"
+    val (datasetBig, rddBig) = readResource(pathBig, onCluster, sqlContext)
+
+    testingInputSize(datasetSmall, rddSmall, "1K")
+    testingInputSize(datasetMedium, rddMedium, "3K")
+    testingInputSize(datasetMedium2, rddMedium2, "5K")
+    testingInputSize(datasetBig, rddBig, "10K")
+
+    testingAnchorSize(datasetSmall, rddSmall, 4)
+    testingAnchorSize(datasetSmall, rddSmall, 10)
+    testingAnchorSize(datasetSmall, rddSmall, 20)
+    testingAnchorSize(datasetSmall, rddSmall, 50)
   }
 
   // Methods
@@ -124,7 +50,7 @@ object Main {
     if (onCluster) {
       input = "../cs422-group13/test/" + file
     } else {
-      val path = "/Users/yawen/Documents/Scala/"
+      val path = "/Users/yawen/Documents/Scala/" 
       //      val path = "../"
       input = new File(path + file).getPath
       // input = new File(getClass.getResource(path).getFile).getPath
